a14b366742a56a77abe7875cbb15e47c688e40b9
PhilippPro <philipp_probst@gmx.de>
2016-03-21 16:33:14 +0100

    actualised Code

--- code/BatchExperiments2.R
+++ code/BatchExperiments2.R
@@ -12,12 +12,14 @@ regis = makeExperimentRegistry(id = "par_randomForest2", packages=c("OpenML", "m
                                work.dir = paste(dir,"/results", sep = ""), src.dirs = paste(dir,"/functions", sep = ""))
 
 # Add problem
-gettask = function(static, idi, rel.nodesize) {
+gettask = function(static, idi, rel.nodesize = 0.0000001, mtry = NULL) {
   task = getOMLTask(task.id = idi, verbosity=0)$input$data.set
+  if (is.null(mtry)) mtry = floor(sqrt(static[static$task_id == idi,]$NumberOfFeatures))
   list(idi = idi, data = task$data, formula = as.formula(paste(task$target.features,"~.") ), 
-       target = task$target.features, mtry_max = static[static$task_id == idi,]$NumberOfFeatures - 1, 
+       target = task$target.features, mtry_max =  - 1, 
        min.node.size = ceiling(rel.nodesize*nrow(task$data)),
-       rel.nodesize = rel.nodesize)
+       rel.nodesize = rel.nodesize,
+       mtry = mtry)
 }
 addProblem(regis, id = "taski", static = tasks, dynamic = gettask, seed = 123)
 
@@ -29,8 +31,8 @@ forest.wrapper.ntree = function(static, dynamic, size = 0, ...) {
   } else {
     err = randomForest(formula = dynamic$formula, data = dynamic$data, replace = TRUE, ...)$mse
   }
-  names(err) = 1:10000
-  list(err = err, datainfo = c(static[static$task_id == dynamic$idi, c(1,2, 15, 13, 14, 18,19)]))
+  list(err = err, datainfo = c(static[static$task_id == dynamic$idi, c(1,2, 15, 13, 14, 18,19)]), 
+       nodesize = dynamic$min.node.size, rel.nodesize = dynamic$rel.nodesize, mtry = dynamic$mtry)
 }
 addAlgorithm(regis, id = "forest.ntree", fun = forest.wrapper.ntree, overwrite = TRUE)
 
@@ -38,7 +40,7 @@ forest.wrapper.parset = function(static, dynamic, ...) {
   if(static[static$task_id == dynamic$idi, 2] == "Supervised Classification") {
     dynamic$data[,dynamic$target] = droplevels(as.factor(dynamic$data[,dynamic$target]))
     pred = ranger(formula = dynamic$formula, data = dynamic$data, replace = TRUE, probability = TRUE, 
-                  min.node.size = dynamic$min.node.size, ... )$predictions
+                  min.node.size = dynamic$min.node.size, mtry = dynamic$mtry, ... )$predictions
     pred2 = factor(colnames(pred)[max.col(pred)], levels = colnames(pred))
     conf.matrix = getConfMatrix2(dynamic, pred2, relative = TRUE)
     k = nrow(conf.matrix)
@@ -50,45 +52,55 @@ forest.wrapper.parset = function(static, dynamic, ...) {
                  measureMMCE(dynamic$data[,dynamic$target], pred2), AUC)
     names(measures) = c("ACC", "BER", "MMCE", "multi.AUC")
   } else {
-    pred = ranger(formula = dynamic$formula, data = dynamic$data, replace = TRUE, min.node.size = dynamic$min.node.size, ...)$predictions
+    pred = ranger(formula = dynamic$formula, data = dynamic$data, replace = TRUE, min.node.size = dynamic$min.node.size, 
+                  mtry = dynamic$mtry, ...)$predictions
     measures = c(measureMAE(dynamic$data[,dynamic$target] , pred),  measureMEDAE(dynamic$data[,dynamic$target], pred), 
                  measureMEDSE(dynamic$data[,dynamic$target], pred), measureMSE(dynamic$data[,dynamic$target], pred))
     names(measures) = c("MAE", "MEDAE", "MEDSE", "MSE")
   }
   list(measures = measures, datainfo = c(static[static$task_id == dynamic$idi, c(1, 2, 15, 13, 14, 18, 19)]), 
-       nodesize = dynamic$min.node.size, rel.nodesize = dynamic$rel.nodesize)
+       nodesize = dynamic$min.node.size, rel.nodesize = dynamic$rel.nodesize, mtry = dynamic$mtry)
 }
 addAlgorithm(regis, id = "forest.parset", fun = forest.wrapper.parset, overwrite = TRUE)
 
 # Für ntree
 pars = list(ntree = 10000)
 forest.design.ntree = makeDesign("forest.ntree", exhaustive = pars)
+pars = list(idi = tasks$task_id)
+task.design = makeDesign("taski", exhaustive = pars)
 
-# Mache ein Dataframe mit allen Parametereinstellungen, die ausprobiert werden sollen
-pars = list(idi = tasks$task_id[1:2], nodesize = c(0.0000001, 1:10)/(10*4)) # nodesize = 1 und dann in regelmäßigen Abständen relativ zu n
-parset.design = data.frame()
-task.design = makeDesign("taski", design = parset.design)
+# Dataframe with all parameter settings, that should be tested
+mtry = numeric()
+for(i in 1:nrow(tasks)) {
+ mtry = c(mtry, 1:c(tasks[i,]$NumberOfFeatures -1 )) # define all mtry values
+}
 
+parset.design = data.frame(idi = rep(tasks$task_id, each = 11), rel.nodesize = rep(c(0.0000001, 1:10)/(10*4), nrow(tasks)), 
+                           mtry = rep(floor(sqrt(tasks$NumberOfFeatures)), each = 11))
+parset.design = rbind(parset.design, data.frame(idi = rep(tasks$task_id, tasks$NumberOfFeatures-1), 
+                                                rel.nodesize = rep(0.0000001, length(mtry)), mtry = mtry))
+task.design2 = makeDesign("taski", design = parset.design)
 
-#task.design = makeDesign("taski", exhaustive = pars)
-
-static = tasks
-
-# pars = list(ntree = 10000)
-# forest.design.mtry = makeDesign("forest.mtry", exhaustive = pars)
 pars = list(num.trees = 10000)
-forest.design.nodesize = makeDesign("forest.nodesize", exhaustive = pars)
+forest.design.parset = makeDesign("forest.parset", exhaustive = pars)
 
 addExperiments(regis, repls = 30, prob.designs = task.design, algo.designs = list(forest.design.ntree)) # 12.5 h 
-addExperiments(regis, repls = 2, prob.designs = task.design, algo.designs = list(forest.design.nodesize)) # 16.5 h
-
+addExperiments(regis, repls = 2, prob.designs = task.design2, algo.designs = list(forest.design.parset)) # 16.5 h
 
 summarizeExperiments(regis)
 testJob(regis)
 
 # Chunk jobs
-id1 = findExperiments(regis, ...)
-submitJobs(regis, ids = )
+chunk1 = list()
+for(i in 1:30)
+chunk1[[i]] = c(findExperiments(regis, algo.pattern = "forest.ntree", repls=i))
+chunk2 = chunk(findExperiments(regis, algo.pattern = "forest.parset"), chunk.size = nrow(tasks))
+
+chunks = c(chunk1, chunk2)
+chunks =  c(findExperiments(regis, algo.pattern = "forest.ntree", repls=1))
+
+submitJobs(regis, ids = 26435)
+#25355
 #waitForJobs(regis)
 
 #regis = loadRegistry("/home/probst/Random_Forest/RFParset/results/par_randomForest-files")
