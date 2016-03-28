6be08ee08a89fa1078cd09801bdf7d5479d7adf3
Philipp Probst <philipp_probst@gmx.de>
2016-03-28 10:25:45 +0200

    updated the cran downloads code

--- code/cran_downloads.R
+++ code/cran_downloads.R
@@ -1,20 +1,24 @@
 library(cranlogs)
 library(data.table)
-library(xtable)
+library(knitr)
 
 downloads = cran_downloads(packages = c("randomForest", "xgboost", "randomForestSRC", "ranger", "Rborist"), when = c("last-month"))
 downloads = data.table(downloads)
-downloads[,sum(count), by = "package"]
-xtable(downloads[,sum(count), by = "package"], digits=0)
+downloads = downloads[,sum(count), by = "package"]
+colnames(downloads) = c("**package**", "**RStudio downloads in the last month**")
+kable(downloads, format = "markdown")
+barplot(downloads[,2], names.arg = downloads$package, col = "blue")
 
 downloads = cran_downloads(packages = c("h2o","ParallelForest", "bigrf"), when = c("last-month"))
 downloads = data.table(downloads)
-downloads[,sum(count), by = "package"]
-xtable(downloads[,sum(count), by = "package"], digits=0)
+downloads = downloads[,sum(count), by = "package"]
+colnames(downloads) = c("**package**", "**RStudio downloads in the last month**")
+kable(downloads, format = "markdown")
 
 downloads = cran_downloads(packages = c("rpart", "RRF", "randomForestSRCSyn", "obliqueRF", "rotationForest", 
                                         "rFerns", "randomUniformForest", "wsrf", "roughrf", "trimTrees", "extraTrees", "party" ), when = c("last-month"))
 downloads = data.table(downloads)
 downloads = downloads[,sum(count), by = "package"]
-downloads[order(downloads$V1, decreasing = T),]
-xtable(downloads[,sum(count), by = "package"], digits=0)
+downloads = downloads[order(downloads$V1, decreasing = T),]
+colnames(downloads) = c("**package**", "**RStudio downloads in the last month**")
+kable(downloads, format = "markdown")
